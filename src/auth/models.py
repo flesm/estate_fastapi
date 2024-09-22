fc9c2764df4bf142c5bdb971539a1a7c11565740
flesm <ostrovskijan659@gmail.com>
2024-09-22 22:40:19 +0300

    db creation and conection

---
+++ src/auth/models.py
@@ -0,0 +1,12 @@
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from sqlalchemy import Column, Integer, String, Boolean
+
+from src.database import Base
+
+
+class User(SQLAlchemyBaseUserTable[int], Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String(length=1024), nullable=False)
+    is_active = Column(Boolean, default=True, nullable=False)
